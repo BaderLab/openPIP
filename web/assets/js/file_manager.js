a0f51c2e0bf85121c668a06a391483d5b005ec80
aniket <ranjan.aniket20013@gmail.com>
2021-07-27 20:20:24 +0530

    file upload: added copy path feature

--- web/assets/js/file_manager.js
+++ web/assets/js/file_manager.js
@@ -1,3 +1,22 @@
 /**
  * 
  */
+
+ function copyFunction(id) {
+    /* Get the text field */
+    console.log(id);
+    var copyText = document.getElementById(id);
+    copyText.style.display = "block";
+  
+    /* Select the text field */
+    copyText.select();
+    console.log(copyText.value);
+    copyText.setSelectionRange(0, 99999); /* For mobile devices */
+  
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+    copyText.style.display = "none";
+  
+    /* Alert the copied text */
+    alert("Copied the text: " + copyText.value);
+  }
