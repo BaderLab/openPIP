9335793b592efa926907c2fee8ef6e2f20840d9a
pa1ncakes <rajtarun782@gmail.com>
2023-12-11 05:29:16 +0000

    updated start.sh and Dockerfile

--- start.sh
+++ start.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-sudo su << SuperUser
-PATH=$PATH:/snap/bin
-chown -R www-data:www-data app/logs &&\
-sudo chmod -R 777 web/uploads &&\ 
-cd .. &&\ 
-docker-compose build &&\ 
+export PATH=$PATH:/snap/bin
+sudo chown -R www-data:www-data app/logs &&
+sudo chmod -R 777 web/uploads &&
+cd ./'Docker OpenPIP package' && 
+docker-compose build &&
 docker-compose up
-SuperUser
