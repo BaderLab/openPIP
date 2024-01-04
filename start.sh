e5dcb3540796bade65bd2e91c4c737ab51b15cdf
Mohamed Helmy <helmy.sfc@gmail.com>
2024-01-04 09:33:59 -0600

    Revert "updated dockerfile, shell script and other files (#85)" (#88)
    This reverts commit d5a970f5cb9cfe1f6ad522cacb27a8a61d91ab74.

--- start.sh
+++ start.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-export PATH=$PATH:/snap/bin
-chown -R www-data:www-data app/logs &&
-	sudo chmod -R 777 web/uploads && \ 
-cd ./'Docker OpenPIP package' && \ 
-docker-compose build && \ 
+sudo su << SuperUser
+PATH=$PATH:/snap/bin
+chown -R www-data:www-data app/logs &&\
+sudo chmod -R 777 web/uploads &&\ 
+cd .. &&\ 
+docker-compose build &&\ 
 docker-compose up
+SuperUser
